75155043dbec53c7394a94bf0c5e4aed50fbb175
lucasbogo <lucasbogo93@gmail.com>
2023-02-10 19:19:47 -0300

    added form fields for adding employee

--- lib/screens/add_employee_screen.dart
+++ lib/screens/add_employee_screen.dart
@@ -26,6 +26,109 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
           ),
         ],
       ),
-      body: const Placeholder(),
+      body: Column(children: [
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Nome',
+            hintText: 'Digite o nome do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Sobrenome',
+            hintText: 'Digite o sobrenome do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Cargo',
+            hintText: 'Digite o cargo do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Salário',
+            hintText: 'Digite o salário do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Data de Nascimento',
+            hintText: 'Digite a data de nascimento do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Data de Admissão',
+            hintText: 'Digite a data de admissão do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Data de Demissão',
+            hintText: 'Digite a data de demissão do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'CPF',
+            hintText: 'Digite o CPF do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'RG',
+            hintText: 'Digite o RG do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Endereço',
+            hintText: 'Digite o endereço do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'Telefone',
+            hintText: 'Digite o telefone do funcionário',
+          ),
+        ),
+        TextFormField(
+          controller: ,
+          keyboardType: TextInputType.name,
+          decoration: const InputDecoration(
+            labelText: 'E-mail',
+            hintText: 'Digite o e-mail do funcionário',
+          ),
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+
+    );  
   }
 }
