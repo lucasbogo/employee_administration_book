ae57f048eca60b565577f877e4a8225287726d2f
lucasbogo <lucasbogo93@gmail.com>
2023-02-10 23:59:46 -0300

    crud operations complete

--- lib/data/local/db/app_db.dart
+++ lib/data/local/db/app_db.dart
@@ -5,6 +5,7 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+
 part 'app_db.g.dart';
 
 LazyDatabase _openConnection() {
@@ -23,4 +24,29 @@ class AppDb extends _$AppDb {
 
   @override
   int get schemaVersion => 1;
+
+  // Buscar a lista de empregados
+  Future<List<EmployeeData>> getEmployees() async {
+    return await select(employee).get();
+  }
+
+  // Buscar um empregado pelo id
+  Future<EmployeeData> getEmployee(int id) async {
+    return await (select(employee)..where((t) => t.id.equals(id))).getSingle();
+  }
+
+  // Atualizar um empregado
+  Future<bool> updateEmployee(EmployeeCompanion entity) async {
+    return await update(this.employee).replace(entity);
+  }
+
+  // Inserir um empregado
+  Future<int> insertEmployee(EmployeeCompanion entity) async {
+    return await into(this.employee).insert(entity);
+  }
+
+  // Deletar um empregado
+  Future<int> deleteEmployee(int id) async {
+    return await (delete(this.employee)..where((t) => t.id.equals(id))).go();
+  }
 }
