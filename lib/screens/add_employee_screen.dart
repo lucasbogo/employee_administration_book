e2acfb1c087fbd8866daa20039801ff6375af876
lucasbogo <lucasbogo93@gmail.com>
2023-02-10 22:01:12 -0300

    custom text form field

--- lib/screens/add_employee_screen.dart
+++ lib/screens/add_employee_screen.dart
@@ -28,113 +28,39 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
           ),
         ],
       ),
-      body: Column(children: [
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Nome',
-            hintText: 'Digite o nome do funcionário',
-          ),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Por favor, digite o nome do funcionário';
-            }
-          },
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Sobrenome',
-            hintText: 'Digite o sobrenome do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Cargo',
-            hintText: 'Digite o cargo do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Salário',
-            hintText: 'Digite o salário do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Data de Nascimento',
-            hintText: 'Digite a data de nascimento do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Data de Admissão',
-            hintText: 'Digite a data de admissão do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Data de Demissão',
-            hintText: 'Digite a data de demissão do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'CPF',
-            hintText: 'Digite o CPF do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'RG',
-            hintText: 'Digite o RG do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Endereço',
-            hintText: 'Digite o endereço do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'Telefone',
-            hintText: 'Digite o telefone do funcionário',
-          ),
-        ),
-        TextFormField(
-          controller: _controller,
-          keyboardType: TextInputType.name,
-          decoration: const InputDecoration(
-            labelText: 'E-mail',
-            hintText: 'Digite o e-mail do funcionário',
-          ),
-        ),
-      ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          CustomTextFormField(controller: _controller),
+        ]),
       ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required TextEditingController controller,
+  }) : _controller = controller;
+
+  final TextEditingController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _controller,
+      keyboardType: TextInputType.name,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Nome',
+        hintText: 'Digite o nome do funcionário',
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor, digite o nome do funcionário';
+        }
+      },
     );
   }
 }
