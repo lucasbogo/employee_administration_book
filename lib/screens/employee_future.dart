27b26dc15c0e01d147ea9c5a8f8f44b5f23602c5
lucasbogo <lucasbogo93@gmail.com>
2023-02-13 20:22:28 -0300

    home screen refactor

---
+++ lib/screens/employee_future.dart
@@ -0,0 +1,94 @@
+import 'package:drift/drift.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+import '../data/local/db/app_db.dart';
+
+class EmployeeFutureScreen extends StatefulWidget {
+  const EmployeeFutureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EmployeeFutureScreen> createState() => _EmployeeFutureScreenState();
+}
+
+class _EmployeeFutureScreenState extends State<EmployeeFutureScreen> {
+  late AppDb _db;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _db = AppDb();
+  }
+
+  @override
+  void dispose() {
+    _db.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Funcionários'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<EmployeeData>>(
+          future: _db.getEmployee(),
+          builder: (context, snapshot) {
+            final List<EmployeeData>? employees = snapshot.data;
+
+            if (snapshot.connectionState != ConnectionState.done) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            if (employees != null) {
+              return ListView.builder(
+                  itemCount: employees.length,
+                  itemBuilder: (context, index) {
+                    final employee = employees[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/edit_employee',
+                            arguments: employee.id);
+                      },
+                      child: Card(
+                        color: Colors.grey[200],
+                        shape: const RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Color.fromARGB(179, 55, 201, 193),
+                              width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(employee.id.toString()),
+                              Text(employee.firstName.toString()),
+                              Text(employee.lastName.toString()),
+                              Text(employee.email.toString()),
+                              Text(employee.phone.toString()),
+                              Text(employee.dateOfBirth.toString()),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            }
+            return const Placeholder();
+          }),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,25 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'package:flutter/material.dart';
+// screens
+import 'employee_future.dart';
+import 'employee_stream.dart';
 
 import '../data/local/db/app_db.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
   late AppDb _db;
+  final pages = [EmployeeFutureScreen(), EmployeeStreamScreen()];
 
   @override
   void initState() {
@@ -35,59 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text('Controle de Funcionários'),
           centerTitle: true,
         ),
-        body: FutureBuilder<List<EmployeeData>>(
-            future: _db.getEmployee(),
-            builder: (context, snapshot) {
-              final List<EmployeeData>? employees = snapshot.data;
-
-              if (snapshot.connectionState != ConnectionState.done) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(snapshot.error.toString()),
-                );
-              }
-              if (employees != null) {
-                return ListView.builder(
-                    itemCount: employees.length,
-                    itemBuilder: (context, index) {
-                      final employee = employees[index];
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/edit_employee',
-                              arguments: employee.id);
-                        },
-                        child: Card(
-                          color: Colors.grey[200],
-                          shape: const RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Color.fromARGB(179, 55, 201, 193),
-                                width: 1),
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(employee.id.toString()),
-                                Text(employee.firstName.toString()),
-                                Text(employee.lastName.toString()),
-                                Text(employee.email.toString()),
-                                Text(employee.phone.toString()),
-                                Text(employee.dateOfBirth.toString()),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    });
-              }
-              return const Placeholder();
-            }),
+        body: pages[index],
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           tooltip: 'Adicionar Funcionário',
@@ -96,23 +50,29 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+          backgroundColor: Color.fromARGB(255, 14, 199, 199),
+          selectedItemColor: Color.fromARGB(255, 13, 138, 21),
+          unselectedItemColor: Color.fromARGB(255, 255, 255, 255),
+          showSelectedLabels: false,
+          showUnselectedLabels: true,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.list),
-              activeIcon: Icon(Icons.list),
+              activeIcon: Icon(Icons.list_alt_outlined),
               label: 'Lista de funcionários',
-            
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              activeIcon: Icon(Icons.business_center_outlined),
+              label: 'Empresa',
+            ),
           ],
-          currentIndex: 0,
-          selectedItemColor: Color.fromARGB(255, 19, 204, 189),
-          onTap: (index) {
-            if (index == 1) {
-              Navigator.pushNamed(context, '/business');
-            }
-            if (index == 2) {
-              Navigator.pushNamed(context, '/school');
-            }
-          },
         ));
   }
 }
