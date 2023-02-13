73d647a8c29195f93eebff205e21f285a6a7b4bb
lucasbogo <lucasbogo93@gmail.com>
2023-02-13 17:46:29 -0300

    copied add employee screen into edit employee. Will fix it later

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -2,14 +2,23 @@ import 'package:employee_book/screens/add_employee_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:employee_book/screens/home_screen.dart';
 import 'package:employee_book/screens/add_employee_screen.dart';
+import 'package:employee_book/screens/edit_employee_screen.dart';
 
 class RouteGenerator {
+  final args = settings.arguments;
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => HomeScreen());
       case '/add_employee':
         return MaterialPageRoute(builder: (_) => AddEmployeeScreen());
+      case '/edit_employee':
+        if (args is int) {
+          return MaterialPageRoute(
+              builder: (_) => EditEmployeeScreen(id: args));
+        }
+        return _errorRoute();
       default:
         return _errorRoute();
     }
--- lib/screens/edit_employee_screen.dart
+++ lib/screens/edit_employee_screen.dart
@@ -19,16 +19,149 @@ class EditEmployeeScreen extends StatefulWidget {
 
 class _EditEmployeeScreenState extends State<EditEmployeeScreen> {
   final _formKey = GlobalKey<FormState>();
+  late AppDb _db;
 
-  late EmployeeData _employee;
+  //late EmployeeData _employee;
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _dateOfBirthController = TextEditingController();
+  DateTime? _dateOfBirth;
+
+  @override
+  void initState() {
+    super.initState();
+    _db = AppDb();
+  }
+
+  @override
+  void dispose() {
+    _db.close();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _dateOfBirthController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Employee'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                addEmployee();
+              },
+              icon: const Icon(Icons.save))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    controller: _firstNameController,
+                    txtLable: 'Nome',
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomTextFormField(
+                    controller: _lastNameController,
+                    txtLable: 'Sobrenome',
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomTextFormField(
+                    controller: _emailController,
+                    txtLable: 'Email',
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomTextFormField(
+                    controller: _phoneController,
+                    txtLable: 'Phone',
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  CustomDatePickerFormField(
+                      controller: _dateOfBirthController,
+                      txtLable: 'Data de Nascimento',
+                      callback: () {
+                        pickDateOfBirth(context);
+                      })
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> pickDateOfBirth(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: _dateOfBirth ?? initialDate,
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2100));
+    builder:
+    (context, child) => Theme(
+        data: ThemeData.light().copyWith(
+          colorScheme: ColorScheme.light(
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+            surface: Colors.blue,
+            onSurface: Colors.black,
+          ),
+          dialogBackgroundColor: Colors.white,
+        ),
+        child: child ?? const Text(''));
+    if (newDate == null) return;
+    setState(() {
+      _dateOfBirth = newDate;
+      String dob = DateFormat('dd/MM/yyyy').format(newDate);
+      _dateOfBirthController.text = dob;
+    });
+  }
+
+  void addEmployee() {
+    final entity = EmployeeCompanion(
+      firstName: drift.Value(_firstNameController.text),
+      lastName: drift.Value(_lastNameController.text),
+      email: drift.Value(_emailController.text),
+      phone: drift.Value(_phoneController.text),
+      dateOfBirth: drift.Value(_dateOfBirth!),
+    );
+    _db.insertEmployee(entity).then((value) =>
+        ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+          backgroundColor: Colors.green,
+          content: const Text(
+            'Funcionário adicionado com sucesso',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () =>
+                    ScaffoldMessenger.of(context).hideCurrentMaterialBanner(),
+                child: const Text(
+                  'Fechar',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        )));
   }
 }
